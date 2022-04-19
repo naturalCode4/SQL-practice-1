c090ecf4d3ac188b4a75ad44304dc737b969e0a9
Adin Kramer <fireowl@Adins-MacBook-Pro.local>
2022-04-19 10:04:49 -0600

    all but delete.sql completed

--- employee.sql
+++ employee.sql
@@ -1,11 +1,33 @@
 -- List all employee first and last names only that live in Calgary.
 
+SELECT first_name, last_name
+FROM employee
+WHERE city = 'Calgary';
+
 -- Find the birthdate for the youngest employee.
+
+SELECT MAX(birth_date)
+FROM employee;
 
 -- Find the birthdate for the oldest employee.
 
+SELECT min(birth_date)
+FROM employee;
+
 -- Find everyone that reports to Nancy Edwards (use the ReportsTo column).
+
+SELECT *
+FROM employee
+WHERE reports_to = 2;
 
 -- You will need to query the employee table to find the id for Nancy Edwards
 
+Select employee_id
+From employee
+WHERE first_name = 'Nancy' AND last_name = 'Edwards';
+
 -- Count how many people live in Lethbridge.
+
+SELECT COUNT(*)
+FROM employee
+WHERE city = 'Lethbridge';
--- group_by.sql
+++ group_by.sql
@@ -1,5 +1,19 @@
-Find the sum of totals in the invoice table grouped by billing_state.
+-- Find the sum of totals in the invoice table grouped by billing_state.
 
-Find the average track length (in milliseconds) by album. Order the table by the averages.
+SELECT billing_state, SUM(total)
+FROM invoice
+GROUP BY billing_state
+ORDER BY billing_state;
 
-Find a count of how many albums the artists with the ids 8 and 22 have respectively. Use COUNT, WHERE/IN, and GROUP BY.
+-- Find the average track length (in milliseconds) by album. Order the table by the averages.
+
+SELECT album_id, AVG(milliseconds)
+FROM track
+GROUP BY album_id
+ORDER BY AVG(milliseconds);
+
+-- Find a count of how many albums the artists with the ids 8 and 22 have respectively. Use COUNT, WHERE/IN, and GROUP BY.
+SELECT COUNT(*)
+FROM album
+WHERE artist_id = 8 OR artist_id = 22
+GROUP BY artist_id;
